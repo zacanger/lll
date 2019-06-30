ac59612a072aca6e47bd292429f7045950795a65
zacanger <zac@zacanger.com>
2019-06-29 18:29:41 -0600

    more cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate cursive;
 
 use cursive::event::EventResult;
 use cursive::traits::*;
-use cursive::views::{DummyView, LinearLayout, OnEventView, SelectView, TextView};
+use cursive::views::{LinearLayout, OnEventView, SelectView, TextView};
 use cursive::Cursive;
 use open::*;
 
@@ -17,6 +17,12 @@ where
 {
     let mut view = SelectView::new();
     for entry in fs::read_dir(directory).expect("can't read directory") {
+        // TODO: sort:
+        // directories
+        // dotfiles
+        // regular files
+        // capitalized comes first
+
         if let Ok(e) = entry {
             let file_name = e.file_name().into_string().unwrap();
             view.add_item(file_name, e);
@@ -24,6 +30,9 @@ where
     }
 
     /*
+    // TODO: make this work
+    // also add l for submit or navigate down
+    // also add h for navigate up
     let view = OnEventView::new(view)
         .on_pre_event_inner('k', |s, _| {
             s.select_up(1);
@@ -35,14 +44,7 @@ where
         });
         */
 
-    view.on_select(update_status).on_submit(open_file)
-}
-
-fn update_status(siv: &mut Cursive, entry: &DirEntry) {
-    let mut status_bar = siv.find_id::<TextView>("status").unwrap();
-    let file_name = entry.file_name().into_string().unwrap();
-    let content = format!("{}", file_name);
-    status_bar.set_content(content);
+    view.on_submit(open_file)
 }
 
 fn open_file(_siv: &mut Cursive, entry: &DirEntry) {
@@ -56,14 +58,8 @@ fn main() {
     let mut panes = LinearLayout::horizontal();
     let picker = file_picker(".");
     panes.add_child(picker.fixed_size((100, 100)));
-    panes.add_child(DummyView);
     let mut layout = LinearLayout::vertical();
     layout.add_child(panes);
-    layout.add_child(
-        TextView::new("status")
-            .with_id("status")
-            .fixed_size((80, 1)),
-    );
     siv.add_layer(layout);
     siv.run();
 }
